40b359a1393d772a0d7886cdea3389a93f17efe8
jmanuel <manuel.gomez.quispe@gmail.com>
2025-07-29 12:09:32 -0500

    feat:updated

--- index.js
+++ index.js
@@ -8,10 +8,22 @@ function getUsers() {
         {"id": 1, "name": "Leanne Graham", "email": "Sincere@april.biz"},
         {"id": 2, "name": "Ervin Howell", "email": "Shanna@melissa.tv"}
       ]`;
- 
+
       resolve(json);
     }, 300);
   });
 }
 
-getUsers().then{(json) => console.log(json)}
+getUsers().then((json) => console.log(json));
+
+getUsers().then((json) => {
+  const users = JSON.parse(json);
+
+  const div = document.querySelector("#users");
+
+  users.forEach((user) => {
+    const p = document.createElement("p");
+    p.textContent = `ID:${user.id} - Nombre:${user.name} - Email: ${user.email}`;
+    div.append(p);
+  });
+});
